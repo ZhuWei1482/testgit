b9f631f2fef44405be696ab49a10fd74f6b06f5d
zhuwei <1511651482@qq.com>
2019-12-30 09:41:12 +0800

    jav编程git练手

--- test/src/test1.java
+++ test/src/test1.java
@@ -5,5 +5,17 @@ public class test1 {
         // 观察x和y是否相等:
         System.out.println(x);
         System.out.println(y);
+        int z = 5>8?5:8;
+        System.out.println(z);
+
+        String s = "hello";
+        String t = s;
+        s = "world";
+        System.out.println(t); // t是"hello"还是"world"?
+
+        String[] names = {"ABC", "XYZ", "zoo"};
+        String w = names[1];
+        names[1] = "cat";
+        System.out.println(w); // s是"XYZ"还是"cat"?
     }
 }
