a6945414cc009b6fc89ebe7b174eb04d70b30ec1
zhuwei <1511651482@qq.com>
2019-12-31 15:47:04 +0800

    日志：SLF4J和Commons Logging的调用方式

--- test/src/main/java/test6.java
+++ test/src/main/java/test6.java
@@ -1,6 +1,12 @@
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class test6 {
+    //注意到实例变量log的获取方式是LogFactory.getLog(getClass())
+    //SLF4J和Commons Logging的调用方式
     public static void main(String[] args) {
-    
+        Log log = LogFactory.getLog(test6.class);
+        log.info("start...");
+        log.warn("end.");
     }
 }
