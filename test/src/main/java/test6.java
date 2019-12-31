6c915b67a26a410f5fe85965f5885ca1ac483c05
zhuwei <1511651482@qq.com>
2019-12-31 15:51:12 +0800

    SLF4J和Logback的日志体系补充

--- test/src/main/java/test6.java
+++ test/src/main/java/test6.java
@@ -4,6 +4,10 @@ import org.apache.commons.logging.LogFactory;
 public class test6 {
     //注意到实例变量log的获取方式是LogFactory.getLog(getClass())
     //SLF4J和Commons Logging的调用方式
+//    接口对比：
+//    Commons Logging	                                                  SLF4J
+//    org.apache.commons.logging.Log	                              org.slf4j.Logger
+//    org.apache.commons.logging.LogFactory	                      org.slf4j.LoggerFactory
     public static void main(String[] args) {
         Log log = LogFactory.getLog(test6.class);
         log.info("start...");
